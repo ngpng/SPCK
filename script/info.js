d675ed645347313e3c4ae1e3616d7a4f0d0cbf9b
ngpng <ngongtran0@gmail.com>
2026-08-31 14:55:44 +0700

    fix some stuff

--- script/info.js
+++ script/info.js
@@ -1,21 +1,24 @@
-const API_KEY = "51ce916d19fbeb3d8481ea109a7109be";
-const IMG_URL = "https://image.tmdb.org/t/p/w500";
+const API_KEY = "38d56cef638f4276a3b2dffe9c3661b0";
+const TRAILER_URL = `https://api.rawg.io/api/games/{id}/movies?key=${API_KEY}`;
 
 const params = new URLSearchParams(window.location.search);
-const movieId = params.get("id");
+const gameId = params.get("id");
 
-if (movieId) {
-  fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${API_KEY}`)
+if (gameId) {
+  fetch(`https://rawg.io/api/games/${gameId}?key=${API_KEY}`)
     .then((res) => res.json())
-    .then((movie) => {
-      document.getElementById("movie-pic").src = IMG_URL + movie.poster_path;
-      document.getElementById("movie-title").innerText = movie.title;
-      document.getElementById("movie-des").innerText =movie.overview || "No des available.";
-      document.getElementById("movie-date").innerText = `Release Date: ${movie.release_date}`;
-
+    .then((game) => {
+      document.getElementById("game-pic").src =game.background_image;
+      document.getElementById("game-title").innerText = game.name;
+      document.getElementById("game-des").innerText = `${game.description_raw || "No description available."}`;
+      document.getElementById("game-date").innerText = `Release Date: ${game.released || "N/A"}`;
+      document.getElementById("game-rating").innerText = `Rating: ${game.rating} / 5⭐`;
+      document.getElementById("game-tag").innerText = `Tags: ${game.genres.map((g) => g.name).join(", ") || "N/A"}`;
 
     })
     .catch((err) => console.log("Lỗi:", err));
 } else {
-  document.getElementById("movie-title").innerText = "Movie not found!";
+  document.getElementById("game-title").innerText = "Game not found!";
 }
+
+
